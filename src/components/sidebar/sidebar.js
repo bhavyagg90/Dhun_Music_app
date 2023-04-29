9e16fa47ba30dc8bab7864e8f3bee46b82a0cb3e
bhavyagg90 <gbhavya18sliet@gmail.com>
2023-04-29 12:51:40 +0530

    second commit library updated

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -11,7 +11,7 @@ export default function Sidebar() {
   const [image, setImage] = useState("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdLAY3C19kL0nV2bI_plU3_YFCtra0dpsYkg&usqp=CAU");
   useEffect(() => {
     apiClient.get("me").then((response) => {
-      console.log(response);
+      setImage(response.data.images[0].url);
     });
   }, []);
   return (
--- src/screens/player/index.js
+++ src/screens/player/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import './player.css'
+import { useLocation } from 'react-router-dom'
 
 export default function Player() {
+
+  const location = useLocation();
+  console.log(location);
+
   return (
     <div className='screen-container'>
-      player
+      <div className="left-player-body"></div>
+      <div className="right-player-body"></div>
     </div>
   )
 }

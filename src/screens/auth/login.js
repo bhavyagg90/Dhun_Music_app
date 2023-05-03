43c8b66e616afe9f9b9be8050d1a919149f1fc3a
bhavyagg90 <gbhavya18sliet@gmail.com>
2023-05-03 15:34:37 +0530

    link updated

--- src/screens/auth/login.js
+++ src/screens/auth/login.js
@@ -6,8 +6,8 @@ export default function Login() {
   return (
     <div className="login-page">
       <img
-        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
-        alt="logo-spotify"
+        src="https://res.cloudinary.com/dbmgq27le/image/upload/v1683108008/m_1_jr4icn.png"
+        alt="logo-Dhun"
         className="logo"
       />
       <a href={loginEndpoint}>
